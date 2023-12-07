b96c491174a8d2de1d62d6949be96ec50d69d2c7
danylonevid@gmail.com <danylonevid@gmail.com>
2023-12-08 00:50:44 +0200

    commit

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(function() {
     $("#first-play").click(function() {
-        window.location.href = "https://danylonevidomyi.github.io/lab7-magic_ball/";
+        window.location.href = "https://danylonevidomyi.github.io/lab7/";
+    })
+
+    $("#second-play").click(function() {
+        window.location.href = "https://danylonevidomyi.github.io/lab8/";
+    })
+
+    $("#third-play").click(function() {
+        window.location.href = "https://danylonevidomyi.github.io/lab9-game-21/";
+    })
+
+    $("#fourth-play").click(function() {
+        window.location.href = "https://danylonevidomyi.github.io/lab10-casino/";
+    })
+
+    $("#fifth-play").click(function() {
+        window.location.href = "https://danylonevidomyi.github.io/lab12/";
     })
 })
